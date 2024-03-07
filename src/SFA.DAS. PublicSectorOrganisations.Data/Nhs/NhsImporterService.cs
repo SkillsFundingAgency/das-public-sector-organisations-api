58347a4eb8e8edfe172e3e6d072bc598d62324de
Paul Graham <paul.graham@coprime.co.uk>
2024-03-07 16:45:49 +0000

    WIP

--- src/SFA.DAS. PublicSectorOrganisations.Data/Nhs/NhsImporterService.cs
+++ src/SFA.DAS. PublicSectorOrganisations.Data/Nhs/NhsImporterService.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Concurrent;
-using System.Data;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.PublicSectorOrganisations.Domain.Configuration;
@@ -28,17 +26,7 @@ public class NhsImporterService : INhsImporterService
 
     public async Task ImportData()
     {
-        var data = new ConcurrentBag<OrganisationSummary>();
-
-        await Parallel.ForEachAsync(_sectors, async (item, ct) =>
-        {
-            var response = await _client.GetAllOrganisations(item.InternalCode);
-
-            foreach (var summary in response.Organisations)
-            {
-                data.Add(summary);
-            }
-        });
+        var data = await FetchSectorSummaries();
 
         var newRecords = new ConcurrentBag<PublicSectorOrganisationEntity>();
         var updateRecords = new ConcurrentBag<PublicSectorOrganisationEntity>();
@@ -46,53 +34,97 @@ public class NhsImporterService : INhsImporterService
         var db = _context.Value;
         var nhsList = await db.PublicSectorOrganisationEntities.Where(x=>x.Source == DataSource.Nhs).AsNoTracking().ToListAsync();
 
-        await Parallel.ForEachAsync(data, async (item, ct) =>
-        {
-            var detail = await _client.GetOrganisation(item.OrgId);
-            var existingEntity = nhsList.FirstOrDefault(x =>
-                x.OrganisationCode.Equals(item.OrgId, StringComparison.InvariantCultureIgnoreCase));
-
-            var record = new PublicSectorOrganisationEntity
-            {
-                Id = existingEntity == null ? Guid.NewGuid() : existingEntity.Id,
-                Name = item.Name,
-                Source = DataSource.Nhs,
-                AddressLine1 = detail.AddressLine1,
-                AddressLine2 = detail.AddressLine2,
-                AddressLine3 = detail.AddressLine3,
-                Town = detail.Town,
-                PostCode = detail.PostCode,
-                Country = detail.Country,
-                UPRN = detail.UPRN,
-                OrganisationCode = item.OrgId,
-                Active = true
-            };
-
-            if (existingEntity != null)
-            {
-                updateRecords.Add(record);
-            }
-            else
-            {
-                newRecords.Add(record);    
-            }
-        });
+        await FetchNewAndExistingDetails(data, nhsList, updateRecords, newRecords);
 
         try
         {
+            _logger.LogInformation("Updating {existingCount} and adding {newCount} NHS Organisations", updateRecords.Count, newRecords.Count);
             await db.Database.BeginTransactionAsync();
             await db.PublicSectorOrganisationEntities.Where(x=>x.Source == DataSource.Nhs).ExecuteUpdateAsync(x => x.SetProperty(x => x.Active, false));
             await db.PublicSectorOrganisationEntities.AddRangeAsync(newRecords);
             db.PublicSectorOrganisationEntities.UpdateRange(updateRecords);
             await db.SaveChangesAsync();
             await db.Database.CommitTransactionAsync();
+            _logger.LogInformation("NHS Update succeeded");
         }
-        catch
+        catch (Exception e)
         {
+            _logger.LogError(e, "NHS Update failed with : {message}", e.Message);
             await db.Database.RollbackTransactionAsync();
             throw;
         }
     }
+
+    private async Task FetchNewAndExistingDetails(ConcurrentBag<OrganisationSummary> data, IReadOnlyCollection<PublicSectorOrganisationEntity> nhsList, ConcurrentBag<PublicSectorOrganisationEntity> updateRecords,
+        ConcurrentBag<PublicSectorOrganisationEntity> newRecords)
+    {
+        try
+        {
+            _logger.LogInformation("Collecting NHS Details for each Organisation");
+            await Parallel.ForEachAsync(data, async (item, ct) =>
+            {
+                var detail = await _client.GetOrganisation(item.OrgId);
+                var existingEntity = nhsList.FirstOrDefault(x =>
+                    x.OrganisationCode.Equals(item.OrgId, StringComparison.InvariantCultureIgnoreCase));
+
+                var record = new PublicSectorOrganisationEntity
+                {
+                    Id = existingEntity == null ? Guid.NewGuid() : existingEntity.Id,
+                    Name = item.Name,
+                    Source = DataSource.Nhs,
+                    AddressLine1 = detail.AddressLine1,
+                    AddressLine2 = detail.AddressLine2,
+                    AddressLine3 = detail.AddressLine3,
+                    Town = detail.Town,
+                    PostCode = detail.PostCode,
+                    Country = detail.Country,
+                    UPRN = detail.UPRN,
+                    OrganisationCode = item.OrgId,
+                    Active = true
+                };
+
+                if (existingEntity != null)
+                {
+                    updateRecords.Add(record);
+                }
+                else
+                {
+                    newRecords.Add(record);
+                }
+            });
+            _logger.LogInformation("Completed collecting NHS Details for each Organisation");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Collecting NHS Details for each Organisation failed with : {message}", e.Message);
+            throw;
+        }
+    }
+
+    private async Task<ConcurrentBag<OrganisationSummary>> FetchSectorSummaries()
+    {
+        var data = new ConcurrentBag<OrganisationSummary>();
+        try
+        {
+            _logger.LogInformation("Collecting NHS Organisations by Sector");
+            await Parallel.ForEachAsync(_sectors, async (item, ct) =>
+            {
+                var response = await _client.GetAllOrganisations(item.InternalCode);
+
+                foreach (var summary in response.Organisations)
+                {
+                    data.Add(summary);
+                }
+            });
+            _logger.LogInformation("Completed collecting NHS Organisations by Sector");
+
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Collecting NHS Organisations by Sector failed with : {message}", e.Message);
+            throw;
+        }
+
+        return data;
+    }
 }
-
-
